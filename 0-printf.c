8b83d39a8c03e81f076721f4a114024f68afb5f2
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-10-16 00:49:47 +0300

    fix the null format

--- 0-printf.c
+++ 0-printf.c
@@ -17,8 +17,10 @@ int _printf(const char *format, ...)
 		{'%', _percent}
 	};
 	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+			return (-1);
 
-	while (*format)
+	while (*format != '\0')
 	{
 		if (*format == '%')
 		{
@@ -32,6 +34,8 @@ int _printf(const char *format, ...)
 			{
 				counter += m[i].f(args);
 			}
+			else
+				return (-1);
 		}
 		else
 		{
